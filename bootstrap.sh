8d5920af0c69d9a3a2ce7839d048285f90037ff1
Barry Rhodes <barry@therhodes.co.uk>
2023-10-18 07:39:23 +0100

    Changes following new Maac setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# @todo make the cmd-shift-l work on install!
+
 # CLI binaries via homebrew
 binaries=(
   nvm
@@ -16,7 +18,6 @@ binaries=(
   zopfli
   ffmpeg
   python
-  sshfs
   trash
   tree
   ack
@@ -24,14 +25,14 @@ binaries=(
   git
   tig
   wget
-  fasd
   peco
 )
+
 # Apps via homebrew cask
 apps=(
   google-chrome
   dbeaver-community
-  spotify
+  # spotify
   orbstack
   iterm2
   visual-studio-code
@@ -50,7 +51,7 @@ apps=(
 
 # fonts to install through cask
 fonts=(
-  font-m-plus
+  font-mplus
   font-clear-sans
   font-roboto
   font-source-code-pro
@@ -62,28 +63,6 @@ packages=(
   pnpm
 )
 
-vscodeExtensions=(
-  Asuka.insertnumbers
-  bmewburn.vscode-intelephense-client
-  cjhowe7.laravel-blade
-  dbaeumer.vscode-eslint
-  ecmel.vscode-html-css
-  eriklynd.json-tools
-  esbenp.prettier-vscode
-  fabianlauer.vs-code-xml-format
-  fabiospampinato.vscode-diff
-  gerane.Theme-TomorrowNightEighties
-  GitHub.copilot
-  GitHub.copilot-chat
-  mohsen1.prettify-json
-  redhat.vscode-xml
-  ryu1kn.partial-diff
-  shufo.vscode-blade-formatter
-  sidthesloth.html5-boilerplate
-  streetsidesoftware.code-spell-checker
-  Tyriar.lorem-ipsum
-  unifiedjs.vscode-mdx
-)
 
 # install homebrew
 if test ! $(which brew); then
@@ -95,35 +74,26 @@ fi
 brew update;
 
 # Install GNU core utilities (those that come with OS X are outdated)
-brew install coreutils
-
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils
-
-# Install Bash 4
-brew install bash
-
-# Install more recent versions of some OS X tools
-brew tap homebrew/dupes
-brew install homebrew/dupes/grep
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed, grep, Bash 4
+brew install coreutils findutils bash grep
 
 echo "installing binaries..."
 brew install ${binaries[@]}
 
 brew cleanup
 
-brew install caskroom/cask/brew-cask
+brew install homebrew/cask/brew-cask
 
 # Install apps to /Applications
 # Default is: /Users/$user/Applications
 echo "installing apps..."
-brew cask install --appdir="/Applications" ${apps[@]}
+brew install --cask --appdir="/Applications" ${apps[@]}
 
-brew tap caskroom/fonts
+brew tap homebrew/cask-fonts
 
 # install fonts
 echo "installing fonts..."
-brew cask install ${fonts[@]}
+brew install --cask ${fonts[@]}
 
 # setup git
 git config --global push.default simple
@@ -134,8 +104,28 @@ nvm install node
 npm install -g ${packages[@]}
 
 # install vscode extensions
-code --install-extension --force ${fonts[@]}
+code --install-extension Asuka.insertnumbers --force
+code --install-extension bmewburn.vscode-intelephense-client --force
+code --install-extension cjhowe7.laravel-blade --force
+code --install-extension dbaeumer.vscode-eslint --force
+code --install-extension ecmel.vscode-html-css --force
+code --install-extension eriklynd.json-tools --force
+code --install-extension esbenp.prettier-vscode --force
+code --install-extension fabianlauer.vs-code-xml-format --force
+code --install-extension fabiospampinato.vscode-diff --force
+code --install-extension gerane.Theme-TomorrowNightEighties --force
+code --install-extension GitHub.copilot --force
+code --install-extension GitHub.copilot-chat --force
+code --install-extension mohsen1.prettify-json --force
+code --install-extension redhat.vscode-xml --force
+code --install-extension ryu1kn.partial-diff --force
+code --install-extension shufo.vscode-blade-formatter --force
+code --install-extension sidthesloth.html5-boilerplate --force
+code --install-extension streetsidesoftware.code-spell-checker --force
+code --install-extension Tyriar.lorem-ipsum --force
+code --install-extension unifiedjs.vscode-mdx --force
 
 mkdir ~/tmp
+cd ~/tmp
 wget "https://gist.githubusercontent.com/brandonb927/3195465/raw/d20ab577180f93da4d9bbea1ddf2a67809c8f69d/osx-for-hackers.sh"
 echo "Downloaded OSX for hackers into ~/tmp/osx-for-hackers.sh. Give it a read through, edit and run it..."
